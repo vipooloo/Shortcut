8cc230f9e943d7fb485470de4cbd47911159c744
caiyuanping <382622713@qq.com>
2026-04-22 23:08:17 +0800

    2026.04.22 23:08

--- src/base/IdAllocator.cpp
+++ src/base/IdAllocator.cpp
@@ -3,35 +3,40 @@
 
 IdAllocator::IdAllocator()
   : m_id_range_rules{
-    std::make_tuple(ShortccutType::SHORTCUT, 1, 1000),
-    std::make_tuple(ShortccutType::EMAIL_DEST, 1001, 2000),
-    std::make_tuple(ShortccutType::FTP_DEST, 2001, 3000),
-    std::make_tuple(ShortccutType::SMB_DEST, 3001, 4000),
-  }
+        std::make_tuple(ShortccutType::SHORTCUT, 1, 1000),
+        std::make_tuple(ShortccutType::EMAIL_DEST, 1001, 2000),
+        std::make_tuple(ShortccutType::FTP_DEST, 2001, 3000),
+        std::make_tuple(ShortccutType::SMB_DEST, 3001, 4000),
+    }
 {
 }
 
 uint32_t
 IdAllocator::AllocateId(ShortccutType type, const std::vector<int>& used_ids)
 {
-  std::vector<IdRangeRule>::const_iterator it = std::find_if(
-    m_id_range_rules.cbegin(),
-    m_id_range_rules.cend(),
-    [type](const IdRangeRule& rule) { return std::get<0>(rule) == type; });
+    std::vector<IdRangeRule>::const_iterator it = std::find_if(
+        m_id_range_rules.cbegin(),
+        m_id_range_rules.cend(),
+        [type](const IdRangeRule& rule) {
+            return std::get<0>(rule) == type;
+        });
 
-  uint32_t allocated_id = 0;
-  if (it != m_id_range_rules.cend()) {
-    uint32_t min_id = std::get<1>(*it);
-    uint32_t max_id = std::get<2>(*it);
+    uint32_t allocated_id = 0;
+    if (it != m_id_range_rules.cend())
+    {
+        uint32_t min_id = std::get<1>(*it);
+        uint32_t max_id = std::get<2>(*it);
 
-    for (uint32_t id = min_id; id <= max_id; ++id) {
-      if (std::find(used_ids.cbegin(), used_ids.cend(), id) ==
-          used_ids.cend()) {
-        allocated_id = id;
-        break;
-      }
+        for (uint32_t id = min_id; id <= max_id; ++id)
+        {
+            if (std::find(used_ids.cbegin(), used_ids.cend(), id) ==
+                used_ids.cend())
+            {
+                allocated_id = id;
+                break;
+            }
+        }
     }
-  }
 
-  return allocated_id;
+    return allocated_id;
 }
---
+++ src/base/ShortcuMgrLogger.h
@@ -0,0 +1,58 @@
+#ifndef SHORTCUMGRLOGGER_H
+#define SHORTCUMGRLOGGER_H
+
+#include <cstdarg>
+#include <cstdio>
+
+// 颜色定义
+#define NONE "\033[0m"
+#define RED "\033[31m"
+#define YELLOW "\033[33m"
+#define GREEN "\033[32m"
+
+// INFO 日志（支持 %d %s %f %x 等）
+static void logInfo(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    std::printf("%s[INFO]  ", GREEN);
+    std::vprintf(format, args);
+    std::printf("%s\n", NONE);
+
+    va_end(args);
+}
+
+// WARN 日志
+static void logWarn(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    std::printf("%s[WARN]  ", YELLOW);
+    std::vprintf(format, args);
+    std::printf("%s\n", NONE);
+
+    va_end(args);
+}
+
+// ERROR 日志
+static void logError(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    std::fprintf(stderr, "%s[ERROR] ", RED);
+    std::vfprintf(stderr, format, args);
+    std::fprintf(stderr, "%s\n", NONE);
+
+    va_end(args);
+}
+
+// 对外使用的宏（和你原来一样）
+#define LOG_INFO(...) logInfo(__VA_ARGS__)
+#define LOG_WARN(...) logWarn(__VA_ARGS__)
+#define LOG_ERROR(...) logError(__VA_ARGS__)
+#define LOG_WARNING(...) logWarn(__VA_ARGS__)
+
+#endif  // SHORTCUMGRLOGGER_H
